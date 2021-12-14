fd1629bad48ca6d6b5ffa5f80c8b2a5ea160dbae
David Herron <david@davidherron.com>
2021-12-13 17:46:15 -0800

    Add sqrt function

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 module.exports.hello = (txt) => {
     console.log(`Hello ${txt}`);
 };
+
+module.exports.square_root = function square_root(n) {
+    let _n;
+    if (Number.isInteger(n)) {
+        _n = Number.parseInt(n);
+        if (Number.isNaN(_n)) {
+            throw new Error(`${n} is an Integer that did not parse`);
+        }
+    } else if (Number.isNaN(Number.parseFloat(n))) {
+        throw new Error(`${n} did not parseFloat`);
+    } else {
+        _n = Number.parseFloat(n);
+    }
+    return Math.sqrt(_n);
+};
